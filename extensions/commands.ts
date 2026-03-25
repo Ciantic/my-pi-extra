f8b119a2f6804918d93c13058f70f333184cb3f7
Jari O. O. Pennanen <ciantic@oksidi.com>
2026-03-25 16:13:34 +0200

    notes...

--- extensions/commands.ts
+++ extensions/commands.ts
@@ -3,7 +3,14 @@ import { AgentSession } from "@mariozechner/pi-coding-agent";
 
 let agentSession: AgentSession;
 
-// My why, there is no better way?
+// My why, there is no better way? PI Extension API has no way to get
+// AgentSession instance, so I monkey patched myself to get the instance.
+//
+// I tried how many times this AgentSession is initiated during interactive PI
+// session. It seems like it is only initiated once. I tried /new and /tree,
+// when navigating or creating a new session it was still initiated only once.
+// It acts like a singleton.
+//
 // @ts-ignore
 const _old = AgentSession.prototype._installAgentToolHooks;
 // @ts-ignore
